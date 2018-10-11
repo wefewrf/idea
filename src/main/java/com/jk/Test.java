baf3b549b97e57919588a2a7deccc99808e38a78
qiqi <jinqin1314520@qq.com>
2018-10-11 18:57:05 +0800

    提交紫霞

--- src/main/java/com/jk/Test.java
+++ src/main/java/com/jk/Test.java
@@ -5,5 +5,6 @@ public class Test<psv> {
         System.out.println("hello !");
         System.out.println("hello2 !");
         System.out.println("hello3 !");
+        System.out.println("卧槽，你是紫霞？");
     }
 }
